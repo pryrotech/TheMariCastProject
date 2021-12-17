940fc44bc9a2192f6c937d975181775c1505fe04
Colby Pryor <61056252+pryrotech@users.noreply.github.com>
2021-12-16 23:55:09 -0330

    Add files via upload

--- MariCast.py
+++ MariCast.py
@@ -1,7 +1,13 @@
+#imports
 import numpy as np
+import requests
+import asyncio
+from env_canada import ECWeather
 from tkinter import *
+import webbrowser
+from pyowm import OWM
 
-
+''''Initiate window and dimensions, display title and description'''
 window = Tk()
 
 
@@ -15,100 +21,179 @@ title.pack()
 
 sub_text = Label(window,text="An application for marine weather reports")
 sub_text.config(font=('Kozuka Gothic Pro M',20),bg=("white"),fg=("blue"))
-sub_text.pack()
+sub_text.pack() #pack the window for display
 
- 
+
 def mainMenu():
-    import asyncio
-    from env_canada import ECWeather
 
-    weather= ECWeather(coordinates=(47.584122, -53.281521))
+    '''Retrieve user's coordinates from IP and city'''
+    ip_request = requests.get('https://get.geojs.io/v1/ip.json')
+    my_ip = ip_request.json()['ip']
+    geo_request = requests.get('https://get.geojs.io/v1/ip/geo/' +my_ip + '.json')
+    geo_data = geo_request.json()
 
-    asyncio.run(weather.update())
+    latitude = float(geo_data['latitude'])
+    longitude = float(geo_data['longitude'])
+    city = str(geo_data['city'])
+    #province = str(geo_data['province'])
+    print(geo_data)
 
-    winds = str(weather.conditions)[594:597]
-    waves = 0
-    visibility = str(weather.conditions)[464:466]
-    sunset = 0
-    sunrise = 0
+    '''Print latitude and longitude'''
+    print(latitude,longitude)
+
+    owm = OWM('')
+    mgr = owm.weather_manager()
+    one_call = mgr.one_call(lat=latitude, lon=longitude)
+
+    temperature = one_call.forecast_daily[0].temperature('celsius').get('min','max')
+
+    winds = one_call.forecast_daily[0].wind().get('speed')
+    
+    waves = 0 #variable for waves
+    
+    visibility = 0 #variable for visibility
+    
+    sunset = 0 #variable for sunset
+    
+    sunrise = 0 #variable for sunrise
+    
     humidex = 0
-    humidity = 0
-    temperature = str(weather.conditions)[50:52]
-    pressure = 0
-    conditions = str(weather.conditions)[1387:1393]
-    forecast = "Mostly cloudy skies tonight, tapering off to clear skies around 00:00."
+    
+    humidity = one_call.current.humidity #variable for humidity
+    
+    pressure = one_call.current.pressure.get('press') #variable for pressure
+    
+    conditions = 0
+
+
+    
+    forecast = "Mostly cloudy skies tonight, tapering off to clear skies around 00:00." #variable for forecast
     
     frame = Frame(window)
-    frame.pack(side="top", expand=True, fill="both")
-    
+    frame.pack(side="top", expand=True, fill="both") #pack the window for display
+
+    #label for current marine weather
     Label(frame,text="Current Marine Weather for: ", font=('Helvetica',15)).place(x=3,y=10)
-    Label(frame,text="St. John's", font=('Helvetica',15),fg=("blue")).place(x=260,y=10)
-    
+    Label(frame,text=city, font=('Helvetica',15),fg=("blue")).place(x=260,y=10)
+
+    #label for winds variable
     Label(frame,text="Winds:", font=('Arial Bold',10)).place(x=3,y=50)
-    
-    '''if(winds > 21):'''
     Label(frame,text=str(winds)+' knots [STRONG]', font=('Arial Bold',12),fg=("red")).place(x=50,y=48)
-    '''elif(winds in range(15,21)):
-        Label(frame,text=str(winds)+' knots [MODERATE]', font=('Arial Bold',12),fg=("orange")).place(x=50,y=48)
-       elif(winds in range(1,14)):
-        Label(frame,text=str(winds)+' knots [LIGHT]', font=('Arial Bold',12),fg=("green")).place(x=50,y=48)'''
 
-        
-    Label(frame,text="Waves:", font=('Arial Bold',10)).place(x=3,y=70)
 
-    if(waves > 3):
-        Label(frame,text=str(waves)+'m  [ROUGH]', font=('Arial Bold',12),fg=("red")).place(x=53,y=68)
-    elif(waves in range(1,2)):
-        Label(frame,text=str(waves)+'m  [MODERATE]', font=('Arial Bold',12),fg=("orange")).place(x=53,y=68)
-    elif(waves in range(0,1)):
-        Label(frame,text=str(waves)+'m  [LIGHT]', font=('Arial Bold',12),fg=("green")).place(x=53,y=68)
-        
+    #label for winds with IF statement to gauge level of winds   
 
+    #label for atmospheric conditions
     Label(frame,text="Conditions: ", font=('Arial Bold',10)).place(x=400,y=50)
     Label(frame,text=conditions, font=('Arial Bold',12)).place(x=480,y=47)
 
+    #label for atmospheric pressure
     Label(frame,text="Atmospheric Pressure: ", font=('Arial Bold',10)).place(x=400,y=70)
     Label(frame,text=str(pressure)+" kPa", font=('Arial Bold',12)).place(x=550,y=68)
 
+    #label for temperature
     Label(frame,text="Temperature: ", font=('Arial Bold',10)).place(x=400,y=90)
     Label(frame,text=str(temperature)+"°C", font=('Arial Bold',12)).place(x=500,y=90)
 
+    #label for humidity
     Label(frame,text="Humidity: ", font=('Arial Bold',10)).place(x=400,y=110)
     Label(frame,text=str(humidity)+"%", font=('Arial Bold',12)).place(x=500,y=110)
-
-    Label(frame,text="Humidex/Windchill: ", font=('Arial Bold',10)).place(x=400,y=130)
-    Label(frame,text=humidex, font=('Arial Bold',12)).place(x=535,y=128)
-
-    Label(frame,text="Sunset/Sunrise:  ", font=('Arial Bold',10)).place(x=400,y=150)
-    Label(frame,text=str(sunset)+"/"+str(sunrise), font=('Arial Bold',12)).place(x=510,y=149)
-
     
+
+    #label for visibility
     Label(frame,text="Visibility:", font=('Arial Bold',10)).place(x=3,y=90)
     Label(frame,text=str(visibility)+' (km)', font=('Arial Bold',12)).place(x=66,y=89)
-    
+
+    #label for forecast
     Label(frame,text="Forecast:", font=('Arial Bold',10)).place(x=3,y=220)
     Label(frame,text=forecast, font=('Arial',8)).place(x=66,y=220)
     
+    #label for risk assessment (good to go?)
     Label(frame,text="Good to go?*: ", font=('Arial Bold',10)).place(x=3,y=280)
     Label(frame,text="*MariCast will factor in winds, waves, and visibility to determine if it is safe for mariners to depart.* ", font=('Arial Italic',10)).place(x=3,y=300)
     
-    '''if(winds > 21 or waves > 3 or visibility < 10):
-        Label(frame,text="DANGEROUS (Do not go) ", font=('Arial Bold',10),bg=("red"),fg=("white")).place(x=100,y=280)
-    elif(winds > 15 or waves in range(1,2) or visibility < 20):
-        Label(frame,text="CAUTION (Take caution, or avoid departing) ", font=('Arial Bold',10),bg=("orange"),fg=("white")).place(x=100,y=280)
-    elif(winds < 15 or waves in range(0,1) or visibility in range(20,100)):
-        Label(frame,text="GOOD (Good to go) ", font=('Arial Bold',10),bg=("green"),fg=("white")).place(x=100,y=280)
-    else:
-        Label(frame,text="GOOD (Good to go) ", font=('Arial Bold',10),bg=("green"),fg=("white")).place(x=100,y=280)'''
+
         
-    Button(frame, text="Calculate ETA", font=('Helvetica bold', 10),bg=("blue"),fg=("white")).place(x=100,y=350)
-
-    Button(frame, text="View Maps", font=('Helvetica bold', 10),bg=("blue"),fg=("white")).place(x=270,y=350)
-
-    Button(frame, text="View Alerts", font=('Helvetica bold', 10),bg=("yellow"),fg=("black")).place(x=430,y=350)
 
 
-title_button_image = PhotoImage(file = "C:/Users/colby/OneDrive/Desktop/Programming Language Research Project/MariCast/title_button.png")
+    Button(frame, text="View Maps", command = viewChart, font=('Helvetica bold', 10),bg=("blue"),fg=("white")).place(x=270,y=350) #button for nautical charts
+
+    alert = Button(frame, text="View Alerts", command= viewAlerts,  font=('Helvetica bold', 10),bg=("yellow"),fg=("black")).place(x=430,y=350) #button for alerts
+
+def viewChart():
+    ip_request = requests.get('https://get.geojs.io/v1/ip.json')
+    my_ip = ip_request.json()['ip']
+    geo_request = requests.get('https://get.geojs.io/v1/ip/geo/' +my_ip + '.json')
+    geo_data = geo_request.json()
+
+    latitude = float(geo_data['latitude'])
+    longitude = float(geo_data['longitude'])
+    
+    url = 'https://fishing-app.gpsnauticalcharts.com/i-boating-fishing-web-app/fishing-marine-charts-navigation.html#12.5/'+str(latitude)+'/'+str(longitude)
+    webbrowser.register('chrome',None,webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
+    webbrowser.get('chrome').open(url)
+
+def viewAlerts():
+    ip_request = requests.get('https://get.geojs.io/v1/ip.json')
+    my_ip = ip_request.json()['ip']
+    geo_request = requests.get('https://get.geojs.io/v1/ip/geo/' +my_ip + '.json')
+    geo_data = geo_request.json()
+    province = str(geo_data['region'])
+    if(province == 'Newfoundland and Labrador'):
+        url = 'https://weather.gc.ca/warnings/index_e.html?prov=nl'
+        webbrowser.register('chrome',None,webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
+        webbrowser.get('chrome').open(url)
+    elif(province == 'Nova Scotia'):
+        url = 'https://weather.gc.ca/warnings/index_e.html?prov=ns'
+        webbrowser.register('chrome',None,webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
+        webbrowser.get('chrome').open(url)
+    elif(province == 'New Brunswick'):
+        url = 'https://weather.gc.ca/warnings/index_e.html?prov=nb'
+        webbrowser.register('chrome',None,webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
+        webbrowser.get('chrome').open(url)
+    elif(province == 'Prince Edward Island'):
+        url = 'https://weather.gc.ca/warnings/index_e.html?prov=pei'
+        webbrowser.register('chrome',None,webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
+        webbrowser.get('chrome').open(url)
+    elif(province == 'Quebec'):
+        url = 'https://weather.gc.ca/warnings/index_e.html?prov=sqc'
+        webbrowser.register('chrome',None,webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
+        webbrowser.get('chrome').open(url)
+    elif(province =='Ontario'):
+        url = 'https://weather.gc.ca/warnings/index_e.html?prov=son'
+        webbrowser.register('chrome',None,webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
+        webbrowser.get('chrome').open(url)
+    elif(province == 'Manitoba'):
+        url = 'https://weather.gc.ca/warnings/index_e.html?prov=mb'
+        webbrowser.register('chrome',None,webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
+        webbrowser.get('chrome').open(url)
+    elif(province == 'Saskatchewan'):
+        url = 'https://weather.gc.ca/warnings/index_e.html?prov=sk'
+        webbrowser.register('chrome',None,webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
+        webbrowser.get('chrome').open(url)
+    elif(province == 'Alberta'):
+        url = 'https://weather.gc.ca/warnings/index_e.html?prov=ab'
+        webbrowser.register('chrome',None,webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
+        webbrowser.get('chrome').open(url)
+    elif(province == 'British Columbia'):
+        url = 'https://weather.gc.ca/warnings/index_e.html?prov=bc'
+        webbrowser.register('chrome',None,webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
+        webbrowser.get('chrome').open(url)
+    elif(province == 'Yukon'):
+        url = 'https://weather.gc.ca/warnings/index_e.html?prov=yt'
+        webbrowser.register('chrome',None,webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
+        webbrowser.get('chrome').open(url)
+    elif(province == 'Northwest Territories'):
+        url = 'https://weather.gc.ca/warnings/index_e.html?prov=nt'
+        webbrowser.register('chrome',None,webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
+        webbrowser.get('chrome').open(url)
+    elif(province == 'Nunavut'):
+        url = 'https://weather.gc.ca/warnings/index_e.html?prov=nu'
+        webbrowser.register('chrome',None,webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
+        webbrowser.get('chrome').open(url)
+        
+#button to continue to main interface
+title_button_image = PhotoImage(file = "C:/Users/colby/OneDrive/Desktop/Programming Language Research Project/MariCast/title_button.png") 
 
 title_button = Button(window,image = title_button_image, borderwidth = 0,command= mainMenu)
 
@@ -116,4 +201,4 @@ title_button.place(x=225,y=150)
 
 
 
-window.mainloop()
+window.mainloop() #loop window
